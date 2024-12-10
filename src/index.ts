f0e322485b9aac238d94d3ba00843d341190385b
giesf <64958447+giesf@users.noreply.github.com>
2024-12-10 16:22:37 +0100

    group by zones

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,18 @@ app.get('/', async (c) => {
   const transparentZones = zones.filter(z => z.endsWith("transparent"))
 
   const rawData = (await $`unbound-control list_local_data`).text()
+  const data = rawData.split("\n").map(l => l.split("\t"))
 
-  return c.text(transparentZones.join("\n") + "\n" + rawData)
+
+  const r = transparentZones.map(z => {
+
+    return `
+      ${z}
+      ${data.filter(d => d[0].endsWith(z.split(" ")[0])).map(d => d.join("\t")).join("\n")}
+    `
+  }).join("\n")
+
+  return c.text(r)
 })
 
 export default app
