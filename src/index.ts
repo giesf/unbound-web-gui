fc29a4964b6ec89d79ae851d71979eb6c9ee6b53
giesf <64958447+giesf@users.noreply.github.com>
2024-12-10 16:18:33 +0100

    Add data

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ app.get('/', async (c) => {
   const zones = rawZones.split("\n")
   const transparentZones = zones.filter(z => z.endsWith("transparent"))
 
-  return c.text(transparentZones.join("\n"))
+  const rawData = (await $`unbound-control list_local_data`).text()
+
+  return c.text(transparentZones.join("\n") + "\n" + rawData)
 })
 
 export default app
